59e13b039481e854f8b54fefb41170a29e3b5451
Yongjie Xue <yxue9@huskers.unl.edu>
2023-11-11 13:49:47 -0600

    refresh token time

--- customers/settings.py
+++ customers/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -61,6 +61,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+     "ROTATE_REFRESH_TOKENS": True
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
